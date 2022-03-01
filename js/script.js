f3a2c09d3ef4b730010cc367c60bbcbb4c134b90
AkashAbrrar <mabrrar9@gmail.com>
2022-03-01 22:04:46 +0600

    error message added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 // get search value 
 const searchPhone = () => {
     const searchField = document.getElementById('search-result');
+    const error = document.getElementById('error');
     const searchText = searchField.value;
     searchField.value = '';
+    if (isNaN(searchText) || searchText === "") {
+        error.innerText = "no results found";
+    }
     // get url data 
     const url = `
     https://openapi.programming-hero.com/api/phones?search=${searchText}
@@ -18,7 +22,7 @@ const displaySearchResult = phones => {
     const searchResult = document.getElementById('search-phone');
     searchResult.innerHTML = '';
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -32,6 +36,7 @@ const displaySearchResult = phones => {
             </div>
          </div>`;
         searchResult.appendChild(div);
+        error.innerText = '';
     })
 
 };
@@ -53,14 +58,25 @@ const showPhoneDetail = mobile => {
     const phoneDetails = document.getElementById('phone-details');
     const box = document.createElement('div');
     box.classList.add('card');
+    phoneDetails.innerText = '';
     box.innerHTML = `
     <img class="w-50" src="${mobile.image}"alt="...">
     <div class="card-body bg-dark">
        <h5 class="card-title text-white">${mobile.brand}</h5>
        <p class="card-text text-white">Model:${mobile.slug}</p>
        <p class="card-text text-white">Release Date:${mobile.releaseDate}</p>
-       <p class="card-text text-white">Release Date:${mobile.mainFeatures}</p>
+       <button onclick="" class="btn btn-info text-white">Explore More</button>
    </div>
     `;
     phoneDetails.appendChild(box);
+};
+
+const addFeaturesPhone = feature => {
+    const url = `
+    https://openapi.programming-hero.com/api/phone/${feature}
+    `;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
 }
+addFeaturesPhone()
